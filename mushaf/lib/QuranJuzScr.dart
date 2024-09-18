82967c409f41496c6cf77a259e974c95bdd69d17
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2024-09-18 06:18:56 +0500

    2 TABS BY PARAH AND TRANSLATIONS

--- mushaf/lib/QuranJuzScr.dart
+++ mushaf/lib/QuranJuzScr.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mushaf/QuranReadScr.dart';
+
 
 import 'package:quran/quran.dart' as quran;
 
